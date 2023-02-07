05576f1d7e98edba0be7a2463582dc6234158c69
Antoine <a.deyris85@gmail.com>
2023-02-07 15:56:47 +0100

    Match case 1 to 3
    Le case est moche mais ca marche

---
+++ RomanConvertor.py
@@ -0,0 +1,17 @@
+
+class RomanConvertor() :
+    arabicNumber = 5
+    
+    def convert(arabicNumber): 
+        
+        match arabicNumber:
+            case 1:
+                return "I"
+            case 2:
+                return "II"
+            case 3:
+                return "III"
+            case _:
+                return "WTF DUDE"
+            
+        
