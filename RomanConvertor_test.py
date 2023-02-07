a4519b06f405298c448a345c4bf33f95e2c9ccab
M.Filiot <gabifiil68@gmail.com>
2023-02-07 15:58:04 +0100

    Change file's name and logic

---
+++ RomanConvertor_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from RomanConvertor import RomanConvertor
+
+class TestRomanConvertor(unittest.TestCase):
+
+    def test_one_is_one(self):
+        convertor = RomanConvertor()
+
+        oneRoman = convertor.convert(1)
+
+        self.assertEqual(oneRoman,"I")
+
+    def test_two_is_two(self):
+        convertor = RomanConvertor()
+
+        twoRoman = convertor.convert(2)
+
+        self.assertEqual(twoRoman, "II")
+    
+    def test_three_is_three(self):
+        convertor = RomanConvertor()
+
+        threeRoman = convertor.convert(2)
+
+        self.assertEqual(threeRoman, "III")
+        
+
+if __name__ == '__main__':
+    unittest.main()
